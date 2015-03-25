9f03d3d656baa0344b285b96fc5c14908318db48
Afonso F <afonso.franca@gmail.com>
2015-03-25 20:22:29 -0300

    Score and some animations

--- script.js
+++ script.js
@@ -1,79 +1,162 @@
+var blueButtonA = 51;
+var blueButtonB = 56;
+var yellowButtonA = 52;
+var yellowButtonB = 50;
+
+var IMAGE_A = 0,
+    IMAGE_B = 1;
+
+
+var playerBlue = {name: "azul", score: 0};
+playerBlue[blueButtonA] = 0;
+playerBlue[blueButtonB] = 1;
+
+var playerYellow = {name: "amarelo", score: 0};
+playerYellow[yellowButtonA] = 0;
+playerYellow[yellowButtonB] = 1;
+
+var players = [playerBlue, playerYellow];
+
+var PRESS_START = 1,
+    QUESTION = 2,
+    ANSWER = 3,
+    RESULT = 4,
+    ANIMATING = 5;
+
+
 var questions = [{
-	images: ['images/question1/img1.jpg', 'images/question1/img2.jpg'],
-	realImage: 1
+    images: ['images/question1/img1.jpg', 'images/question1/img2.jpg'],
+    realImage: IMAGE_B
+}, {
+    images: ['images/question1/img1.jpg', 'images/question1/img2.jpg'],
+    realImage: IMAGE_B
+}, {
+    images: ['images/question1/img1.jpg', 'images/question1/img2.jpg'],
+    realImage: IMAGE_B
+}, {
+    images: ['images/question1/img1.jpg', 'images/question1/img2.jpg'],
+    realImage: IMAGE_B
+}, {
+    images: ['images/question1/img1.jpg', 'images/question1/img2.jpg'],
+    realImage: IMAGE_B
 }];
 
-var players = [{
-	49: 0,
-	50: 1
-},
-{
-	51: 0,
-	52: 1
-}];
 
-var questionId = -1;
+var status = PRESS_START;
+var questionId = 0;
 var question;
+var result;
 
-$(document).ready(function(){
-	printStartScreen();
-});
-
-function nextQuestion(){
-	question = questions[questionId];
-	printQuestion(question);
-}
-
-$(window).on('keypress', function(key){
-	if(questionId == -1){
-		questionId = 0;
-		nextQuestion();
-		return;
-	}
-	var result = getResult(question, key.keyCode);
-	if(result) {
-		alert("Player " + result.player + " " + (result.match?"acertou":"errou"));
-		questionId++;
-		question = questions[questionId];
-		if(question) {
-			nextQuestion();
-		}
-		else{
-			alert('There\'s no more questions');
-			questionId = -1;
-			printStartScreen();
-		}
-	}
+$(document).ready(function () {
+    render();
 });
 
 
+$(window).on('keydown', function (key) {
+    if (status == PRESS_START) {
+        status = QUESTION;
+        question = questions[questionId];
+        questionId++;
+        render();
+    }
+    else if (status == QUESTION) {
+        result = getResult(question, key.keyCode);
+        if (result) {
+            status = ANSWER;
 
-function getResult(question, key){
-	var playerId = getPlayer(key);
-	if(players[playerId][key] == question.realImage) {
-			return { player: playerId, match: true };
-	}
-	else {
-			return { player: playerId, match: false };
-	}
-		
+            var otherPlayer = (1 - result.player);
+            if (result.match) {
+                players[result.player].score++;
+            }
+            else {
+                players[otherPlayer].score++;
+            }
+            render();
+        }
+    }
+    /*else if (status == ANSWER) {
+        question = questions[questionId];
+        questionId++;
+        if (question) {
+            status = QUESTION;
+            render();
+        }
+        else {
+            status = RESULT;
+            render();
+        }
+    }*/
+    else if (status == RESULT) {
+        status = PRESS_START;
+        players.forEach(function (p) {
+            p.score = 0;
+        });
+        questionId = 0;
+        render();
+    }
+});
+
+
+function getResult(question, key) {
+    var playerId = getPlayer(key);
+    if (playerId === undefined) return null;
+    if (players[playerId][key] == question.realImage) {
+        return {player: playerId, match: true};
+    }
+    else {
+        return {player: playerId, match: false};
+    }
+
 }
 
 function getPlayer(key) {
-	for(var i = 0; i < players.length; i++) {
-		if(players[i][key] !== undefined) {
-			return i;
-		}
-	}
+    for (var i = 0; i < players.length; i++) {
+        if (players[i][key] !== undefined) {
+            return i;
+        }
+    }
 }
 
 function printQuestion(question) {
-	$('#images').html('');
-	question.images.forEach(function(image){
-		$('#images').append('<img src="'+ image + '">');
-	});
+    $('#question').html('<h1>Pergunta numero ' + (questionId) + "</h1>");
+    question.images.forEach(function (image) {
+        $('#question').append('<img src="' + image + '">');
+    });
 }
 
-function printStartScreen(){
-	$('#images').html('Pressione qualquer tecla para comecar');
+function render() {
+    if (status == PRESS_START) {
+        $('#pressStart').fadeIn('slow');
+    }
+    else if (status == QUESTION) {
+        $('#pressStart').fadeOut('slow');
+        $('#answer').hide();
+        printQuestion(question);
+        $('#question').show();
+    }
+    else if (status == ANSWER){
+        $('#answer').html("<h1>Player " + players[result.player].name + " " + (result.match ? "acertou" : "errou") + "!</h1>");
+        status = ANIMATING;
+        $('#answer').slideDown('slow', function(){
+            setTimeout(function() {
+                question = questions[questionId];
+                questionId++;
+                if (question) {
+                    status = QUESTION;
+                }
+                else {
+                    status = RESULT;
+                }
+                render();
+            }, 3000);
+        });
+
+    }
+    else if (status == RESULT) {
+        $('#question').hide();
+        $('#answer').hide();
+        var $result = $('#result');
+        $result.html('<h1>Resultado final</h1>' + JSON.stringify(players));
+        $result.fadeIn('slow');
+    }
 }
